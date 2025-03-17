04a45d9b2ce008884170bf7440a94f922e6a1a74
Imrane-Hub5 <imrane.maadoum@gmail.com>
2025-03-17 15:19:29 +0100

    Update connexion.php

--- app/views/utilisateurs/connexion.php
+++ app/views/utilisateurs/connexion.php
@@ -1,12 +1,5 @@
 <?php include 'includes/header.php'; ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EasyStage - Connexion</title>
-    <link rel="stylesheet" href="public\assets\css\style.css">
-</head>
+
 <body>
     <header class="header">
         <div class="logo">EasyStage</div>
