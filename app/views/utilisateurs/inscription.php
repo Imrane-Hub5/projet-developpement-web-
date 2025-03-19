c8f2fed8aa1abf411bb5bb24a544fe8746dbda27
Imrane-Hub5 <imrane.maadoum@gmail.com>
2025-03-19 10:35:39 +0100

    Update inscription.php

--- app/views/utilisateurs/inscription.php
+++ app/views/utilisateurs/inscription.php
@@ -1,10 +1,11 @@
+<?php include('../includes/header.php'); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EasyStage - Inscription</title>
-    <link rel="stylesheet" href="public\assets\css\style.css">
+    
 </head>
 <body>
     <header class="header">
