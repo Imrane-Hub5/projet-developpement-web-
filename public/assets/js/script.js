3b4d47c3baa795119767fd1310149b7b3ddb03a0
melxbnz <melissa.benyezzar@gmail.com>
2025-03-30 01:00:51 +0100

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Erreur : Impossible de trouver les éléments du menu !");
     }
 });
-//-------------------------------------------------------------------------------------------------------------MENU BURGER-
+//--------------------------------------------------------------------------------------MENU BURGER-
 
 
 
