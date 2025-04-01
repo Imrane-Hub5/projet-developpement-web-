34a2ac354f5688b22b40d5036039dacc3810d48e
Imrane-Hub5 <imrane.maadoum@gmail.com>
2025-04-01 10:51:05 +0200

    Create confirmation.php

---
+++ app/views/utilisateurs/confirmation.php
@@ -0,0 +1,11 @@
+<?php include('app\views\includes\header.php'); ?>
+
+<main class="confirmation-container">
+    <div class="confirmation-card">
+        <h2>🎉 Inscription réussie !</h2>
+        <p>Votre compte a été créé avec succès.</p>
+        <a href="connexion.php" class="btn-continue">Se connecter</a>
+    </div>
+</main>
+
+<?php include('app\views\includes\footer.php'); ?>
