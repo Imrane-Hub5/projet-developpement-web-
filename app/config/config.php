a6f4ca771e6b9ec029c55dbe6455a86d1a44c881
melxbnz <melissa.benyezzar@gmail.com>
2025-04-02 00:31:57 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,4 @@
 <?php
+require_once __DIR__ . '/Database.php';
 
-$host = 'localhost';
-$dbname = 'easystage';
-$username = 'root';
-$password = ''; 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-?>
+$pdo = Database::connect(); 
