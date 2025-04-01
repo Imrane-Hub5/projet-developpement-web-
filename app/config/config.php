94b37bdcfb7881be17c4a927d39dc544ac07dbd5
melxbnz <melissa.benyezzar@gmail.com>
2025-04-01 16:24:20 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'easystage');
-define('DB_USER', 'root');
-define('DB_PASS', '');  
-
+$host = 'localhost';
+$dbname = 'easystage';
+$username = 'root';
+$password = ''; 
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, [
